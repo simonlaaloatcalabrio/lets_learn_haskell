f7212e19089a806708614741377c4d3f39bfadad
Simon Laalo <simon.laalo@calabrio.com>
2020-03-02 11:53:52 -0800

    Probably broken initial project setup for Luke Evan's Haskell course at Calabrio

---
+++ llh/src/SessionOne.hs
@@ -0,0 +1,59 @@
+module SessionOne where
+
+import Data.Char
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+  putStrLn (show (quadroot 1 2 3))
+  putStrLn (show (multiplicationTable 10))
+  putStrLn (capitalizeWords "i've seen things you wouldn't believe")
+  putStrLn (capitalizeWords2 "attackships on fire off the shoulder of orion")
+  putStrLn (capitalizeWords3 "time to die")
+
+
+quadroot :: Double -> Double->Double->(Double,Double)
+quadroot a b c = 
+  let 
+    d = sqrt b^2-4*a*c
+  in
+    (1/(2*a) * ((-1)* b) + d, 1/(2*a) * ((-1)* b) - d)
+
+multiplicationTable :: Int-> [[Int]]
+multiplicationTable limit = 
+  let
+    header = [1..limit]
+    cols h x = map (x*) h
+  in
+    map (cols header) header
+
+capitalizeWords :: [Char]->[Char]
+capitalizeWords source = 
+  let
+    capitalizeWord (w:t) = toUpper w : t
+  in
+    unwords $ map capitalizeWord $ words source
+
+capitalizeWords2 :: String -> String
+capitalizeWords2 source = 
+  let 
+    capitalizeWord w =  toUpper (head w) : (tail w)
+  in
+    unwords [capitalizeWord w | w <- (words source)]
+
+
+capitalizeWords3 :: String -> String
+capitalizeWords3 source =
+  let 
+    capitalizeWord (w:ws) =  toUpper w : ws
+    capitalizeWord [] = []
+    foo :: String -> [String]
+    foo x  = do
+      curWord <- words x
+      return $ capitalizeWord curWord
+  in unwords $ foo source
+      
+  
+ 
+
+  
